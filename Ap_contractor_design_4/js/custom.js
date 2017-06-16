49ccaf4ff1b98ffca87528f69899c02f863ec199
Ankita Verma <ankitaverma1000@gmail.com>
2017-06-16 14:42:09 +0530

    Contractor website

---
+++ Ap_contractor_design_4/js/custom.js
@@ -0,0 +1,14 @@
+/* Add your custom JavaScript code */
+// $(document).ready(function () {
+// 	    $('.owl-slider-style4').owlCarousel({
+// 				loop :true,
+//         navigation: false, // Show next and prev buttons
+//         slideSpeed: 300,
+//         items: 4,
+//         itemsDesktop: [1200, 4],
+//         itemsTablet: [991, 3],
+//         itemsMobile: [767, 1],
+//         paginationSpeed: 400,
+//         navigationText: ['<i class="fa fa-long-arrow-left"></i>', '<i class="fa fa-long-arrow-right"></i>']
+//     });
+// });
